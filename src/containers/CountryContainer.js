84273de7ae1542f7bc6e5cebfd8188be3dceec15
Jacob <callaghanjacob@gmail.com>
2022-04-07 11:33:15 +0100

    components tab reciving appropriate data from API

--- src/containers/CountryContainer.js
+++ src/containers/CountryContainer.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React,{ useState, useEffect } from "react";
 
 const CountryContainer = () => {
 
+    const[countries, setCountries] = useState([]);
+
+    useEffect(() => {
+        getCountries();
+    },[])
+
+    const getCountries = function(){
+        fetch('https://restcountries.com/v2/all')
+        .then(res => res.json())
+        // .then(country => console.log(country))
+        .then(countries => setCountries(countries))
+    }
+
+
+
+
+
     return (
-        <p>I will be a list</p>
+        
     );
 };
 
